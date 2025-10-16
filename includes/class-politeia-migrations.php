362ae56fbdc6db538257150ef41ee776c9fa3f8d
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-10-16 12:18:17 -0300

    Add wp_politeia_turnout table for election participation data

---
+++ includes/class-politeia-migrations.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Loads database migrations for Politeia Electoral Map.
+ *
+ * @package Politeia
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+        exit;
+}
+
+require_once __DIR__ . '/migrations/2025-10-15-add-turnout-table.php';
